8e2a84a62a3e344031a0db47538b25b53efc6651
Davide Marchiori <davide.melkior@gmail.com>
2021-10-27 17:29:43 +0200

    First version of class utente
    First version of abstract class utente with attributes and methods

--- src/main/java/Utente.java
+++ src/main/java/Utente.java
@@ -1,4 +1,25 @@
 
 public abstract class Utente {
-
+	
+	private String CF;
+	private String name;
+	private String surname;
+	
+	public void setCF(String CF) throws Exception {
+		if(!CF.matches(("/^[A-Z]{6}\\d{2}[A-Z]\\d{2}[A-Z]\\d{3}[A-Z]$/i")))
+			throw new Exception("Formato codice fiscale non valido");
+	}
+	
+	public String getCF() {
+		return this.CF;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getSurname() {
+		return this.surname;
+	}
+	
 }
