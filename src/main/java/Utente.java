ed08cf50e1a680f2384c35cfbed4d262d525c963
Davide Marchiori <davide.melkior@gmail.com>
2021-10-27 18:34:06 +0200

    added constructor for class Utente

--- src/main/java/Utente.java
+++ src/main/java/Utente.java
@@ -5,6 +5,12 @@ public abstract class Utente {
 	private String name;
 	private String surname;
 	
+	public Utente(String CF, String name, String surname) throws Exception {
+		setCF(CF);
+		this.name = name;
+		this.surname = surname;
+	}
+	
 	public void setCF(String CF) throws Exception {
 		if(!CF.matches(("/^[A-Z]{6}\\d{2}[A-Z]\\d{2}[A-Z]\\d{3}[A-Z]$/i")))
 			throw new Exception("Formato codice fiscale non valido");
