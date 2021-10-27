67a9f79c27892c4d89200ec4712093e79305a50f
Mirko Morello <mirkomorello97@gmail.com>
2021-10-27 18:31:47 +0200

    Added Expiration date for Election

--- src/main/java/Elezione.java
+++ src/main/java/Elezione.java
@@ -3,4 +3,8 @@ import java.time.LocalTime;
 public abstract class Elezione {
 	private String id;
 	private LocalTime expire;
+	
+	public LocalTime getExpirationDate(){
+		return this.expire;
+	}
 }
