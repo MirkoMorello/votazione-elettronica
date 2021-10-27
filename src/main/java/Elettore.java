ed8a5eb19ea8cf21e2048f7e1663111dd18c54f1
Davide Marchiori <davide.melkior@gmail.com>
2021-10-27 18:35:27 +0200

    added methods to class Elettore
    added methods to check if the user can vote, added constructor

--- src/main/java/Elettore.java
+++ src/main/java/Elettore.java
@@ -1,15 +1,30 @@
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Elettore extends Utente{
 	
-	public void Vote(Elezione election, Voto voto) throws Exception {
-		if (HasVoted(election)) {
+	private LocalDate born_on;
+	
+	public Elettore(String CF, String name, String surname, LocalDate born_on) throws Exception {
+		super(CF, name, surname);
+		this.born_on = born_on;
+		// TODO Auto-generated constructor stub
+	}
+
+	public void vote(Elezione election, Voto voto) throws Exception {
+		if (this.hasVoted(election)) {
 			return;
 		}
 		//TODO aggiungere sistema di votazione effettivo
 	}
 	
-	private boolean HasVoted(Elezione election){
-		//TODO: aggiungere sistema di identificazione per voto già effettuato
+	public boolean hasVoted(Elezione election){
+		//TODO: aggiungere sistema di identificazione per voto giï¿½ effettuato
 		return false;
+	}
+	
+	public boolean canVote() {
+		int years = Period.between(born_on, LocalDate.now()).getYears();
+		return (years >= 18);
 	}
 }
