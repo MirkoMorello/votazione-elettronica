b73d8935c0623dbb235024fb23fd671c4d551f41
Davide Marchiori <davide.melkior@gmail.com>
2021-11-12 17:45:58 +0100

    Began assignment 3

--- src/main/java/Scrutatore.java
+++ src/main/java/Scrutatore.java
@@ -2,23 +2,23 @@ import java.time.LocalDate;
 
 public class Scrutatore extends Utente {
 
-	public Scrutatore(String CF, String name, String surname) throws Exception {
-		super(CF, name, surname);
+	public Scrutatore( String name, String surname) throws Exception {
+		super(name, surname);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void userVoted(String CF, String name, String surname, LocalDate born, Elezione election) throws Exception {
-		Elettore e = new Elettore(CF, name, surname, born);
-		if (e.canVote()) {
-			if(e.hasVoted(election)) {
-				throw new Exception("User already voted");
-			}
-			/* TODO lo scrutatore, quando si presenta un elettore in presenza
-			 * deve registrare che tale elettore ha votato, in modo tale che, se si dovesse
-			 * collegare da remoto, il sistema non gli permetta di votare una seconda volta 
-			 */
-		} else {
-			throw new Exception("User cannot vote");
-		}
-	}
+//	public void userVoted(String CF, String name, String surname, LocalDate born, Elezione election) throws Exception {
+//		Elettore e = new Elettore(name, surname, born);
+//		if (e.canVote()) {
+//			if(e.hasVoted(election)) {
+//				throw new Exception("User already voted");
+//			}
+//			/* TODO lo scrutatore, quando si presenta un elettore in presenza
+//			 * deve registrare che tale elettore ha votato, in modo tale che, se si dovesse
+//			 * collegare da remoto, il sistema non gli permetta di votare una seconda volta 
+//			 */
+//		} else {
+//			throw new Exception("User cannot vote");
+//		}
+//	}
 }
