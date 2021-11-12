070f4044033e195394f61784f8094b14df383983
Davide Marchiori <davide.melkior@gmail.com>
2021-11-12 17:45:58 +0100

    Began assignment 3

--- src/main/java/Elettore.java
+++ src/main/java/Elettore.java
@@ -3,11 +3,28 @@ import java.time.Period;
 
 public class Elettore extends Utente{
 	
-	private LocalDate born_on;
+	private char[] CF;
 	
-	public Elettore(String CF, String name, String surname, LocalDate born_on) throws Exception {
-		super(CF, name, surname);
-		this.born_on = born_on;
+	private LocalDate nascita;
+	private String comune;
+	private char[] nazione;
+	public enum sesso {M, F};
+	private sesso sesso;
+	private boolean voto;
+	
+	
+	public Elettore(String CF, String name, String surname, LocalDate nascita, String comune, String nazione, sesso sesso) throws Exception {
+		super(name, surname);
+		this.nazione = new char[2];
+		this.nascita = nascita;
+		this.comune = comune;
+		if(nazione.length() != 2) {
+			throw new Exception("Indicare La nazione con il corrispettivo codice di due caratteri (esempio: Italia - IT)");
+		}
+		this.nazione[0] = nazione.charAt(0);
+		this.nazione[1] = nazione.charAt(1);
+		this.sesso = sesso;
+		setCF(CF);
 		// TODO Auto-generated constructor stub
 	}
 
@@ -24,7 +41,21 @@ public class Elettore extends Utente{
 	}
 	
 	public boolean canVote() {
-		int years = Period.between(born_on, LocalDate.now()).getYears();
+		int years = Period.between(nascita, LocalDate.now()).getYears();
 		return (years >= 18);
+	}
+	
+	
+	
+	public void setCF(String CF) throws Exception {
+		this.CF = new char[16];
+		for(int i = 0; i < CF.length(); i++) {
+			this.CF[i] = CF.charAt(i);
+		}
+	}
+	
+	private boolean checkCF(String CF) {
+		
+		return true;
 	}
 }
--- src/main/java/Utente.java
+++ src/main/java/Utente.java
@@ -1,23 +1,12 @@
 
 public abstract class Utente {
 	
-	private String CF;
 	private String name;
 	private String surname;
 	
-	public Utente(String CF, String name, String surname) throws Exception {
-		setCF(CF);
+	public Utente(String name, String surname) throws Exception {
 		this.name = name;
 		this.surname = surname;
-	}
-	
-	public void setCF(String CF) throws Exception {
-		if(!CF.matches(("/^[A-Z]{6}\\d{2}[A-Z]\\d{2}[A-Z]\\d{3}[A-Z]$/i")))
-			throw new Exception("Formato codice fiscale non valido");
-	}
-	
-	public String getCF() {
-		return this.CF;
 	}
 	
 	public String getName() {
@@ -27,5 +16,7 @@ public abstract class Utente {
 	public String getSurname() {
 		return this.surname;
 	}
+	
+	
 	
 }
