39d292639d05352e1eda59d7efbbb5d571c22676
Davide Marchiori <davide.melkior@gmail.com>
2021-11-14 21:02:49 +0100

    added JML

--- src/main/java/Elettore.java
+++ src/main/java/Elettore.java
@@ -2,65 +2,72 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public class Elettore extends Utente{
+	/*@ public invariant 
+	 @ (sesso == 'm' || sesso == 'f');
+	 @ (nazione.equals("italia")) ==> comune != null;
+	 @ nascita.before(LocalDate.now());
+	 @ checkCF() == true
+	 @*/
 	
-	private char[] CF;
+	/*
+	 * Gli attributi nome e cognome sono contenuti nella superclasse "Utente"
+	 */
 	
-	private LocalDate nascita;
-	private String comune;
-	private char[] nazione;
-	public enum sesso {M, F};
-	private sesso sesso;
-	private boolean voto;
+	private /*@ spec_public @*/ char[] CF;
+	private /*@ spec_public @*/ LocalDate nascita;
+	private /*@ spec_public @*/ String comune;
+	private /*@ spec_public @*/ String nazione;
+	private /*@ spec_public @*/ char sesso;
+	private /*@ spec_public @*/ boolean voto;
 	
 	
-	public Elettore(String CF, String name, String surname, LocalDate nascita, String comune, String nazione, sesso sesso) throws Exception {
+	public Elettore(String CF, String name, String surname, LocalDate nascita, String comune, String nazione, char sesso) throws Exception {
 		super(name, surname);
-		this.nazione = new char[2];
 		this.voto = false;
-		this.nascita = nascita;
+		this.nascita = LocalDate.of(nascita.getYear(), nascita.getMonth(), nascita.getDayOfMonth());
 		this.comune = comune;
-		if(nazione.length() != 2) {
-			throw new Exception("Indicare La nazione con il corrispettivo codice di due caratteri (esempio: Italia - IT)");
-		}
-		this.nazione[0] = nazione.charAt(0);
-		this.nazione[1] = nazione.charAt(1);
-		this.sesso = sesso;
+		this.nazione = nazione.toLowerCase().trim();
+		this.sesso = Character.toLowerCase(sesso);
 		setCF(CF);
-		// TODO Auto-generated constructor stub
-	}
-
-	public void vote(Elezione election, Voto voto) throws Exception {
-		if (this.hasVoted(election)) {
-			return;
-		}
-		//TODO aggiungere sistema di votazione effettivo
 	}
 	
-	public boolean hasVoted(Elezione election){
-		//TODO: aggiungere sistema di identificazione per voto gi� effettuato
-		return false;
-	}
-	
-	public boolean canVote() {
+	public /*@ pure @*/ boolean canVote() {
 		int years = Period.between(nascita, LocalDate.now()).getYears();
 		return (years >= 18);
 	}
 	
-	
-	
 	public void setCF(String CF) throws Exception {
+		if(CF.length() != 16) {
+			throw new Exception("La lunghezza del codice fiscale deve essere di 16 caratteri");
+		}
 		this.CF = new char[16];
 		for(int i = 0; i < CF.length(); i++) {
 			this.CF[i] = CF.charAt(i);
 		}
 	}
 	
-	private boolean checkCF(String CF) {
-		
+	private /*@ pure @*/ boolean checkCF() {
+		if(!nazione.equals("italia")) {
+			if(this.CF[11] != 'Z') {
+				return false;
+			}
+		}
+		if(!Character.isAlphabetic(this.CF[11])) {
+			return false;
+		}
+		if(!(Character.isDigit(this.CF[12]) && Character.isDigit(this.CF[13]) && Character.isDigit(this.CF[14]))){
+			return false;
+		}
+		if(!Character.isAlphabetic(this.CF[15])) {
+			return false;
+		}
 		return true;
+		
 	}
 	
+	//@ requires this.canVote() && this.voto == false;
 	private void esprimiVoto() {
 		this.voto = true;
 	}
+
 }
