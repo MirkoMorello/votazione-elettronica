1fa14b01014dd7260a5df49ff4311a85c4558b47
Davide Marchiori <davide.melkior@gmail.com>
2021-12-20 18:01:03 +0100

    added interface ElettoreDao

---
+++ src/main/java/ElettoreDaoImpl.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class ElettoreDaoImpl implements ElettoreDao{
+	
+	public List<Elettore> getAllElettori() {
+		
+		return null;
+	}
+
+	public Elettore getElettore(char[] CF) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean DeleteElettore(char[] CF) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean AddElettore(Elettore e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
