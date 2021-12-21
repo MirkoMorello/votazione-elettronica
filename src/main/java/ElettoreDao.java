89ce990e57f125635b4ce1938d76d3f7c058f983
Davide Marchiori <davide.melkior@gmail.com>
2021-12-21 15:49:34 +0100

    added connection to database

--- src/main/java/ElettoreDao.java
+++ src/main/java/ElettoreDao.java
@@ -1,8 +1,8 @@
 import java.util.List;
 
 public interface ElettoreDao {
-	public List<Elettore> getAllElettori();
-	public Elettore getElettore(char[] CF);
+	public List<Elettore> getAllElettori() throws Exception;
+	public Elettore getElettore(char[] CF) throws Exception;
 	public boolean DeleteElettore(char[] CF);
 	public boolean AddElettore(Elettore e);
 }
